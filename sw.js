b5fba92d4f000697dd41c4c00860750c709c82b6
mkt-AN <mkt@injincorp.com>
2026-05-13 15:18:42 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'injin-v1';
+const assets = ['./', 'Estimator 101.html', 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
